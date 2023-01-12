2c950b60fecc43a571ff1514984e627a735f575c
Sourav <soukumarr@gmail.com>
2023-01-12 19:00:06 +0530

    Changed UI

--- server.js
+++ server.js
@@ -8,11 +8,36 @@ const jwt = require('jsonwebtoken')
 
 const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
 
-mongoose.connect('mongodb://localhost:27017/login-app-db', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true
-})
+
+
+// mongoose.connect('mongodb://localhost:27017/login-app-db', {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// 	useCreateIndex: true
+// })
+
+const uri = 'mongodb://localhost:27017/test';
+// const uri = "mongodb://0.0.0.0:27017/";
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+}
+
+const connectWithDB = () => {
+    mongoose.connect(uri, options, (err, db) => {
+      if (err) console.error(err);
+      else console.log("database connection")
+    })
+}
+
+connectWithDB()
 
 const app = express()
 app.use('/', express.static(path.join(__dirname, 'static')))
@@ -78,7 +103,7 @@ app.post('/api/login', async (req, res) => {
 })
 
 app.post('/api/register', async (req, res) => {
-	const { username, password: plainTextPassword } = req.body
+	const { username, email, password: plainTextPassword } = req.body
 
 	if (!username || typeof username !== 'string') {
 		return res.json({ status: 'error', error: 'Invalid username' })
@@ -100,6 +125,7 @@ app.post('/api/register', async (req, res) => {
 	try {
 		const response = await User.create({
 			username,
+			email,
 			password
 		})
 		console.log('User created successfully: ', response)
